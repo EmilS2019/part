0dac886bb745f5933cedf292e75b47ea51fb7350
Kattenelvis <officialkattenelvis@gmail.com>
2019-08-23 22:15:43 +0200

    Changed from phonebook to main

--- my-app/PhoneBook/src/index.js
+++ my-app/PhoneBook/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 //import axios from 'axios'
 import backend from './backend'
 
-  /*const CheckForDuplicate = (theArray, item) =>{
+  const CheckForDuplicate = (theArray, item) =>{
 
     for (let i = 0; i < theArray.length; i++) {
       const element = theArray[i];
@@ -16,7 +16,19 @@ import backend from './backend'
 
     }
     return true
-  }*/
+  }
+
+  const Notification = ({message}) =>{
+
+    if(message === null) return null
+
+    return (
+        <div className="notification" id={message.state}>
+            {message.message}
+        </div>
+
+    )
+}
 
 const App = () => {
 
@@ -29,16 +41,30 @@ const App = () => {
           key={name.id}     
           name={name.name}
           pNumber={name.pNumber}
-          deleteNumber ={() => deleteNum(name.id)}
-          />   
+          deleteNumber ={() => deleteNum(name.id)}/>   
           )
-
 
   const [ newName, setNewName ] = useState('') 
   const [ newPNum, setNewPNum ] = useState('') 
   const [ persons, setPersons ] = useState([]) 
   const [showAll, setShowAll] = useState([])  
   const [searchTerm, setSearchTerm] = useState('')  
+  const [notificationMessage, setNotificationMessage] = useState(null)
+
+  const resetNotificationAfterTime = (timeInSeconds) =>{
+    setTimeout(() =>{
+      setNotificationMessage(null)
+    }, timeInSeconds * 1000)
+  }
+
+  const displayNotification = (returnednumber, typeOfChange, timeInSeconds) =>{
+    setNotificationMessage(
+      {message:`succesfully ${typeOfChange}: "${returnednumber.name}" 
+       with the number "${returnednumber.pNumber}"`, 
+       state: "success"}
+      )
+      resetNotificationAfterTime(timeInSeconds)
+  }
 
   useEffect(() => {
     backend.getNumbers()
@@ -46,7 +72,8 @@ const App = () => {
       setPersons(persons)
       setShowAll(persons)
     })
- }, [])
+  }, [])
+  
 
   const handleSearch = (event) => {
     let searchValue = event.target.value
@@ -61,51 +88,68 @@ const App = () => {
       person.name.toUpperCase().includes(term.toUpperCase()) 
       || person.pNumber.includes(term.toUpperCase())))
   }
+  
+  //This is to prevent an error 500 due to two numbers having the same ID 
+  const highestID = () =>{
+    let currentHighestID = 0;
+    persons.forEach(number => {
+      if (number.id > currentHighestID) currentHighestID = number.id
+    })
+    return currentHighestID
+  }
+  let newID = highestID() + 1
+  
 
-  let newID = persons.length + 1
+  const handleSubmit = (event) => 
+  {
+      event.preventDefault()
+      
+      const number = {
+          id: newID,
+          name: newName,
+          pNumber: newPNum
+      }
 
-    const handleSubmit = (event) => 
-    {
-        event.preventDefault()
-        
-        const number = {
-            id: newID,
-            name: newName,
-            pNumber: newPNum
-        }
+      let replaceExistingName = false;
+      let replaceWithThisID = 0;
+      persons.forEach(num => {
 
-        let replaceExistingName = false;
-        let replaceWithThisID = 0;
-        console.log(persons)
-        persons.forEach(num => {
+          if (num.name === number.name){
+              replaceExistingName = true
+              replaceWithThisID = num.id
+          }
+      })
 
-            if (num.name === number.name){
-                replaceExistingName = true
-                replaceWithThisID = num.id
-            }
+      if (replaceExistingName === false){    
+          const addnum = backend.addNumber(number)
+          addnum.then(returnednumber => {
+          setPersons(persons.concat(returnednumber))
+          setShowAll(persons.concat(returnednumber))
+          newID++
+          displayNotification(returnednumber, "added", 8)
         })
-
-        if (!replaceExistingName){    
-            const addnum = backend.addNumber(number)
-            addnum.then(returnednumber => {
-            setPersons(persons.concat(returnednumber))
-            setShowAll(persons.concat(returnednumber))
-            newID++})
-        }
-        else
-        {
-            const addnum = backend.editNumber(number, replaceWithThisID)
-            addnum.then(returnednumber => 
-            {
-                persons[replaceWithThisID - 1] = returnednumber
-                setPersons(persons)
-                console.log(persons)
-                setShowAll(persons)
+      }
+      else
+      {
+          const addnum = backend.editNumber(number, replaceWithThisID)
+          addnum.then(returnednumber => 
+          {
+            persons.forEach(person => {
+              if (person.id === replaceWithThisID) person = returnednumber})
+              backend.getNumbers().then(returnedArray =>{
+                setPersons(returnedArray)
+                setShowAll(returnedArray)
+              })
+              setShowAll(persons)
+              displayNotification(returnednumber, "edited", 8)
             })
-        }                               
-        setNewName('')
-        setNewPNum('')
-    }
+          .catch(error =>{
+            console.log(error)
+          })
+      }                               
+      setNewName('')
+      setNewPNum('')
+  }
   
   const handleNameChange = (event) =>{
     setNewName(event.target.value)
@@ -115,15 +159,24 @@ const App = () => {
     setNewPNum(event.target.value)
   }
 
+  let personToDelete=null
   const deleteNum = (id) =>{
     if(window.confirm("Are you sure you wan't to delete this number?")){
         backend.deleteNumber(id).then(
-            setPersons(persons.filter(person => 
-                person.id === id ? false : true
-            )),
-            setShowAll(persons.filter(person => 
-                person.id === id ? false : true
-            )))
+            
+          persons.forEach(person => {
+            if (person.id === id){
+              personToDelete = person
+            }
+          }),
+          setPersons(persons.filter(person => 
+              person === personToDelete ? false : true
+          )),
+          setShowAll(persons.filter(person => 
+              person === personToDelete ? false : true
+          )),
+          displayNotification(personToDelete, "deleted", 8)
+          )
     }}
       
   return (
@@ -133,12 +186,13 @@ const App = () => {
       <br/>
 
       <h2>Add new Number</h2>
+
+      <Notification message={notificationMessage}/>
+
       <form onSubmit={handleSubmit}>      
         <div>Name: <input value={newName} onChange={handleNameChange}/></div>
         <div>Number: <input value={newPNum} onChange={handlePNumChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div><button type="submit">add</button></div>
       </form>
 
       <br/>
@@ -147,7 +201,7 @@ const App = () => {
       <ul>
         {rows(showAll)}
       </ul>
-      <button >debug</button>
+      <button>debug</button>
     </div>
   )
 }
